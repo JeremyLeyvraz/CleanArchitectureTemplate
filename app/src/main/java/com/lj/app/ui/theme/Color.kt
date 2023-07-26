d7b686a91932327c469651163b1ec5bbf8ed7894
Jérémy Leyvraz <32959624+JeremyLeyvraz@users.noreply.github.com>
2023-07-26 10:34:48 +0200

    Add and fix detekt (#2)
    Co-authored-by: Jeremy Leyvraz <jeremy.leyvraz@diabeloop.fr>

--- app/src/main/java/com/lj/app/ui/theme/Color.kt
+++ app/src/main/java/com/lj/app/ui/theme/Color.kt
@@ -2,7 +2,12 @@ package com.lj.app.ui.theme
 
 import androidx.compose.ui.graphics.Color
 
-val Purple200 = Color(0xFFBB86FC)
-val Purple500 = Color(0xFF6200EE)
-val Purple700 = Color(0xFF3700B3)
-val Teal200 = Color(0xFF03DAC5)
+const val PURPLE_200 = 0xFFBB86FC
+const val PURPLE_500 = 0xFF6200EE
+const val PURPLE_700 = 0xFF3700B3
+const val TEAL_200 = 0xFF03DAC5
+
+val Purple200 = Color(PURPLE_200)
+val Purple500 = Color(PURPLE_500)
+val Purple700 = Color(PURPLE_700)
+val Teal200 = Color(TEAL_200)
