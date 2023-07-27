2b1acbd26905620aee5312b0ab88e8060ade16dc
Jeremy Leyvraz <jeremy.leyvraz@diabeloop.fr>
2023-07-27 11:45:41 +0200

    android test works

--- data/src/androidTest/java/com/lj/data/ExampleInstrumentedTest.kt
+++ data/src/androidTest/java/com/lj/data/ExampleInstrumentedTest.kt
@@ -19,6 +19,6 @@ class ExampleInstrumentedTest {
     fun useAppContext() {
         // Context of the app under test.
         val appContext = InstrumentationRegistry.getInstrumentation().targetContext
-        assertEquals("com.lj.usecase.test", appContext.packageName)
+        assertEquals("com.lj.data.test", appContext.packageName)
     }
 }
